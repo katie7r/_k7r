6507bf739e15abac1514d2d0e99dd8e672478b21
Katie Russ <katie.k7r@gmail.com>
2015-07-03 14:45:37 -0400

    Trying to fix devise admin redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :store_location
+
+  # Helpers of sorts
+
+  private
+
+  def store_location
+    return unless request.get?
+    session[:previous_url] = request.fullpath unless request.path.include?('/admin/') || request.xhr?
+  end
+
+  def default_redirect
+    session[:previous_url] || root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    default_redirect
+  end
+
+  def after_sign_out_path_for(resource)
+    default_redirect
+  end
+
+  def restrict_to_admin
+    unless current_admin
+      flash[:alert] = 'You are not authorized to perform this action.'
+      return redirect_to default_redirect
+    end
+  end
 end
